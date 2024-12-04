596a927e4c27c11ead913f485946ebc53d7b0eb2
Lucas Costa <lucas.desenvolvedor.tec@gmail.com>
2024-12-04 13:23:28 -0300

    Implementing possible moves of Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -4,7 +4,7 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece{
+public class Rook extends ChessPiece {
 
 	public Rook(Board board, Color color) {
 		super(board, color);
@@ -14,5 +14,18 @@ public class Rook extends ChessPiece{
 	public String toString() {
 		return "R";
 	}
-
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		// above
+				p.setValues(position.getRow() - 1, position.getColumn());
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setRow(p.getRow() - 1);
+				}
+	}
 }
